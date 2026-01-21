14586be6863100195c02e0f972f7d13d75fc5352
Ahmed Mohamed <abdelmonaem535@gmail.com>
2026-01-21 21:52:29 +0200

    socket config

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json({ limit: "2mb" }));
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("server is running on " + PORT);
   connectDB();
 });
