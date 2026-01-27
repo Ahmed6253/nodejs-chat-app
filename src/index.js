c1e06dbf16596b616719841a2e7784e08fb47f90
Ahmed Mohamed <abdelmonaem535@gmail.com>
2026-01-27 23:58:45 +0200

    feat: add production static file serving and path resolution

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -19,6 +22,13 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("server is running on " + PORT);
